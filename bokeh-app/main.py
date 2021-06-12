9d154a087a2a91ef609f453e45a4ac305839e589
RiveSunder <RiveSunder@protonmail.com>
2021-06-12 12:28:35 -0600

    modify bokeh-app for showcasing found mobile patterns

--- bokeh-app/main.py
+++ bokeh-app/main.py
@@ -1,4 +1,5 @@
-from bokeh.io import curdoc
+import os
+
 
 import numpy as np
 import torch
@@ -6,9 +7,11 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from carle.env import CARLE
-from game_of_carle.agents.submission_agents import SubmissionAgent
+from carle.mcl import SpeedDetector
+from game_of_carle.agents.toggle import Toggle
 
 import bokeh
+from bokeh.io import curdoc
 import bokeh.io as bio
 from bokeh.io import output_notebook, show
 from bokeh.plotting import figure
@@ -18,61 +21,95 @@ from bokeh.models import TextInput, Button, Paragraph
 from bokeh.models import ColumnDataSource
 from bokeh.events import DoubleTap, Tap
 
+agent =  Toggle()
+policy_list = []
+directory_list = os.listdir("policies/")
 
-env = CARLE()
+for filename in directory_list:
+    
+    if "Toggle" in filename and "glider" in filename:
+        
+        policy_list.append(os.path.join("policies", filename))
+        
+# instantiate CARLE with a speed detection wrapper
+env = CARLE(height=256, width=256)
+env = SpeedDetector(env)
 
-agent = SubmissionAgent()
-agent.toggle_rate = 0.48
+# set Move/Morley rules
+my_rules = "B368/S245"
 
-env.birth = [3]
-env.survive = [0,2,3] 
+agent.set_params(np.load(policy_list[0]))
+
+print(f"{len(policy_list)} Toggle glider policies found.")
+
+env = CARLE(height=256, width=256)
+env.rules_from_string(my_rules)
+env = SpeedDetector(env)
 
 global obs
 obs = env.reset()
 
-p = figure(plot_width=3*256, plot_height=3*256)
+p = figure(plot_width=3*256, plot_height=3*256, title="CA Universe")
+p_plot = figure(plot_width=int(2.5*256), plot_height=int(2.5*256), title="'Reward'")
 
 global my_period
+global number_agents
+global agent_number
+
+agent_number = 0
+number_agents = len(policy_list)
 my_period = 512
 
-# add a circle renderer with x and y coordinates, size, color, and alpha
-
 source = ColumnDataSource(data=dict(my_image=[obs.squeeze().cpu().numpy()]))
-ColumnDataSource(data=dict(x=[1], y=[0]))
+source_plot = ColumnDataSource(data=dict(x=np.arange(1), y=np.arange(1)*0))
+
 img = p.image(image='my_image',x=0, y=0, dw=256, dh=256, palette="Magma256", source=source)
+line_plot = p_plot.line(line_width=3, color="firebrick", source=source_plot)
 
 button_go = Button(sizing_mode="stretch_width", label="Run >")     
 button_slower = Button(sizing_mode="stretch_width",label="<< Slower")
 button_faster = Button(sizing_mode="stretch_width",label="Faster >>")
-button_reset = Button(sizing_mode="stretch_width",label="Reset")
-message = Paragraph()
+button_reset_prev_agent = Button(sizing_mode="stretch_width",label="Reset w/ Prev. Agent")
+button_reset_this_agent = Button(sizing_mode="stretch_width",label="Reset w/ This Agent")
+button_reset_next_agent = Button(sizing_mode="stretch_width",label="Reset w/ Next Agent")
 
-input_birth = TextInput(value="B3")
-input_survive = TextInput(value="S023")
-button_birth = Button(sizing_mode="stretch_width", label="Update Birth Rule")
-button_survive = Button(sizing_mode="stretch_width", label="Update Survival Rule")
 button_agent_switch = Button(sizing_mode="stretch_width", label="Turn Agent Off")
 
+message = Paragraph()
+
 def update():
-    global obs
-    global stretch_pixel
-    global action
-
-    obs, r, d, i = env.step(action)
-
-    if agent_on:
-        action = agent(obs)
-    else:
-        action = torch.zeros_like(action)
-
-    padded_action = stretch_pixel/2 + env.action_padding(action).squeeze()
-
-    my_img = (padded_action*2 + obs.squeeze()).cpu().numpy()
-    my_img[my_img > 3] = 3.0
-
-    new_data = dict(my_image=[my_img])
-    
-    source.stream(new_data, rollover=1)  
+        global obs
+        global stretch_pixel
+        global action
+        global agent_on
+        global my_step
+        global rewards
+        global agent_number
+        
+        obs, r, d, i = env.step(action)
+        rewards = np.append(rewards, r.cpu().numpy().item())
+        if agent_on:
+            action = agent(obs) 
+        else:
+            action = torch.zeros_like(action)
+            
+        #padded_action = stretch_pixel/2 + env.action_padding(action).squeeze()
+        padded_action = stretch_pixel/2 + env.inner_env.action_padding(action).squeeze()
+        
+        my_img = (padded_action*2 + obs.squeeze()).cpu().numpy()
+        my_img[my_img > 3.0] = 3.0
+        (padded_action*2 + obs.squeeze()).cpu().numpy()
+        new_data = dict(my_image=[my_img])
+        
+        #new_line = dict(x=np.arange(my_step+2), y=rewards)
+        new_line = dict(x=[my_step], y=[r.cpu().numpy().item()])
+        
+        source.stream(new_data, rollover=1)
+        source_plot.stream(new_line, rollover=2000)
+        
+        my_step += 1
+        message.text = f"agent {agent_number}, step {my_step}, reward: {r.item()} \n"\
+                f"{policy_list[agent_number]}"
     
 def go():
    
@@ -86,7 +123,7 @@ def go():
 
 def faster():
     global my_period
-    my_period = max([my_period * 0.5, 1])
+    my_period = max([my_period / 2, 1])
     go()
     go()
     
@@ -96,136 +133,180 @@ def slower():
     go()
     go()
 
-def reset():
+def reset_this_agent():
     global obs
     global stretch_pixel
-
-    obs = env.reset()
+    global my_step
+    global rewards
+    global agent_number
+    global number_agents
+    
+    my_step = 0
+    
+    obs = env.reset()        
+    agent.reset()
+            
     stretch_pixel = torch.zeros_like(obs).squeeze()
     stretch_pixel[0,0] = 3
-
     new_data = dict(my_image=[(stretch_pixel + obs.squeeze()).cpu().numpy()])
-
-    source.stream(new_data, rollover=1)
-
-def set_birth_rules():
-    env.birth_rule_from_string(input_birth.value)
-
-    my_message = "Rules updated to B"
-
-    for elem in env.birth:
-        my_message += str(elem)
-
-    my_message += "/S"
-
-    for elem in env.survive:
-        my_message += str(elem)
-
-    message.text = my_message
-
-def set_survive_rules():
-    env.survive_rule_from_string(input_survive.value)
-
-    my_message = "Rules updated to B"
-
-    for elem in env.birth:
-        my_message += str(elem)
-
-    my_message += "/S"
-
-    for elem in env.survive:
-        my_message += str(elem)
-
-    message.text = my_message
+    rewards = np.array([0])
     
+    new_line = dict(x=[my_step], y=[0])
+    
+    source_plot.stream(new_line, rollover=1)
+    source.stream(new_data, rollover=8)
+ 
+def reset_next_agent():
+   
+    global obs
+    global stretch_pixel
+    global my_step
+    global rewards
+    global agent_number
+    global number_agents
+    
+    my_step = 0
+    
+    obs = env.reset()        
+            
+    stretch_pixel = torch.zeros_like(obs).squeeze()
+    stretch_pixel[0,0] = 3
+    new_data = dict(my_image=[(stretch_pixel + obs.squeeze()).cpu().numpy()])
+    rewards = np.array([0])
+    
+    new_line = dict(x=[my_step], y=[0])
+    
+    source_plot.stream(new_line, rollover=1)
+    source.stream(new_data, rollover=8)
+    
+    agent_number = (agent_number + 1) % number_agents
+    
+    agent.set_params(np.load(policy_list[agent_number]))
+    agent.reset()
+    
+    message.text = f"reset with agent {agent_number}"
+        
+def reset_prev_agent():
+   
+    global obs
+    global stretch_pixel
+    global my_step
+    global rewards
+    global agent_number
+    global number_agents
+    
+    my_step = 0
+    
+    obs = env.reset()        
+            
+    stretch_pixel = torch.zeros_like(obs).squeeze()
+    stretch_pixel[0,0] = 3
+    new_data = dict(my_image=[(stretch_pixel + obs.squeeze()).cpu().numpy()])
+    rewards = np.array([0])
+    
+    new_line = dict(x=[my_step], y=[0])
+    
+    source_plot.stream(new_line, rollover=1)
+    source.stream(new_data, rollover=8)
+    
+    agent_number = (agent_number - 1) % number_agents
+    
+    agent.set_params(np.load(policy_list[agent_number]))
+    agent.reset()
+    
+    message.text = f"reset with agent {agent_number}"
+
 def human_toggle(event):
     global action
-    
-    coords = [np.round(event.y -0.5), np.round(event.x-0.5)]
+
+    coords = [np.round(env.height*event.y/256-0.5), np.round(env.width*event.x/256-0.5)]
     offset_x = (env.height - env.action_height) / 2
     offset_y = (env.width - env.action_width) / 2
-    
+
+    print(offset_x, coords[0])
     coords[0] = coords[0] - offset_x
     coords[1] = coords[1] - offset_y
-    
+
+    print(offset_x, coords)
     coords[0] = np.uint8(np.clip(coords[0], 0, env.action_height-1))
     coords[1] = np.uint8(np.clip(coords[1], 0, env.action_height-1))
-    
+
     action[:, :, coords[0], coords[1]] = 1.0 * (not(action[:, :, coords[0], coords[1]]))
-    
-    padded_action = stretch_pixel/2 + env.action_padding(action).squeeze()
-    
+
+    print(offset_x, coords[0])
+    #padded_action = stretch_pixel/2 + env.action_padding(action).squeeze()
+    padded_action = stretch_pixel/2 + env.inner_env.action_padding(action).squeeze()
+
     my_img = (padded_action*2 + obs.squeeze()).cpu().numpy()
     my_img[my_img > 3.0] = 3.0
     (padded_action*2 + obs.squeeze()).cpu().numpy()
     new_data = dict(my_image=[my_img])
-    
-    source.stream(new_data, rollover=1)
-    
+
+    source.stream(new_data, rollover=8)
+
 def clear_toggles():
     global action
-    
-    action *= 0
+
     if button_go.label == "Pause":
-        curdoc().remove_periodic_callback(curdoc().session_callbacks[0])
+
+        action *= 0
+        doc.remove_periodic_callback(doc.session_callbacks[0])
         button_go.label = "Run >"
 
-        padded_action = stretch_pixel/2 + env.action_padding(action * 0).squeeze()
-        
+        #padded_action = stretch_pixel/2 + env.action_padding(action * 0).squeeze()
+        padded_action = stretch_pixel/2 + env.inner_env.action_padding(action).squeeze()
+
         my_img = (padded_action*2 + obs.squeeze()).cpu().numpy()
         my_img[my_img > 3.0] = 3.0
         (padded_action*2 + obs.squeeze()).cpu().numpy()
         new_data = dict(my_image=[my_img])
-        
-        source.stream(new_data, rollover=1)
-    else:
-        my_callback = curdoc().add_periodic_callback(update, my_period)
 
+        source.stream(new_data, rollover=8)
+    else:
+        doc.add_periodic_callback(update, my_period)
         button_go.label = "Pause"
 
 def agent_on_off():
     global agent_on
-
+    
     if button_agent_switch.label == "Turn Agent Off":
-
         agent_on = False
         button_agent_switch.label = "Turn Agent On"
-
+            
     else:
-
         agent_on = True
         button_agent_switch.label = "Turn Agent Off"
-    
+
 global agent_on
 agent_on = True
-
-
 global action
 action = torch.zeros(1, 1, env.action_height, env.action_width)
 
-reset()
+reset_this_agent()
 
 p.on_event(Tap, human_toggle)
 p.on_event(DoubleTap, clear_toggles)
 
-reset()
+button_reset_prev_agent.on_click(reset_prev_agent)
+button_reset_this_agent.on_click(reset_this_agent)
+button_reset_next_agent.on_click(reset_next_agent)
 
-button_birth.on_click(set_birth_rules)
-button_survive.on_click(set_survive_rules)
 button_go.on_click(go)
 button_faster.on_click(faster)
 button_slower.on_click(slower)
-button_reset.on_click(reset)
 button_agent_switch.on_click(agent_on_off)
 
-control_layout = row(button_slower, button_go, button_faster, button_reset)
-rule_layout = row(input_birth, button_birth, input_survive, button_survive)
-display_layout = row(p)
+display_layout = row(p, p_plot)
+control_layout = row(button_slower, button_go, button_faster)
+toggle_toggle_agent_layout = row(button_reset_prev_agent, \
+        button_reset_this_agent, \
+        button_reset_next_agent)
+
 message_layout = row(message)
 agent_toggle_layout = row(button_agent_switch)
 
 curdoc().add_root(display_layout)
 curdoc().add_root(control_layout)
-curdoc().add_root(rule_layout)
+curdoc().add_root(toggle_toggle_agent_layout)
 curdoc().add_root(message_layout)
 curdoc().add_root(agent_toggle_layout)
